9767f37ddebab77d8935540a3c3821d1c3f65ac9
iyue <ys1231@126.com>
2024-04-14 20:05:07 +0800

    build(docs): :hammer_and_wrench: Add compilation dependency

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Disable FLAG_SECURE
 Disable `FLAG_SECURE` on all windows, enabling screenshots in apps that normally wouldn\'t allow it, and disabling screenshot detection on Android 14+.
 
+## init env
+
+```shell
+mkdir libxposed
+
+git clone https://github.com/libxposed/api.git
+cd api
+echo 'org.gradle.caching=true' >> gradle.properties
+echo 'org.gradle.parallel=true' >> gradle.properties
+echo 'org.gradle.vfs.watch=true' >> gradle.properties
+echo 'org.gradle.jvmargs=-Xmx2048m' >> gradle.properties
+./gradlew publishToMavenLocal
+cd ..
+
+git clone https://github.com/libxposed/service.git
+cd service
+# 报错 多运行几次 或者删除缓存文件后继续  ~/.gradle/caches
+./gradlew publishToMavenLocal
+cd ..
+```
+
 ## Usage
 1. Enable the module
 2. Select **ONLY** recommended apps
 3. Reboot
+
+
--- app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
+++ app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
@@ -20,26 +20,44 @@ import io.github.libxposed.api.annotations.XposedHooker;
 
 @SuppressLint({"PrivateApi", "BlockedPrivateApi"})
 public class DisableFlagSecure extends XposedModule {
-
-    public DisableFlagSecure(XposedInterface base, ModuleLoadedParam param) {
+    /**
+     * 实例化新的 Xposed 模块。<br/>
+     * 当模块加载到目标进程中时，将调用构造函数。
+     *
+     * @param基础框架提供的实现接口，模块不宜使用
+     * @param参数 有关模块加载过程的信息
+     */
+    public DisableFlagSecure(@NonNull XposedInterface base, @NonNull ModuleLoadedParam param) {
         super(base, param);
     }
 
+    // 构造函数
+
+
+    /**
+     * 系统服务加载时的处理逻辑。
+     * 主要用于反射调用和修改系统内部行为，以禁用屏幕安全标志。
+     *
+     * @param param 包含系统服务类加载器的对象。
+     */
     @Override
     public void onSystemServerLoaded(@NonNull SystemServerLoadedParam param) {
         var classLoader = param.getClassLoader();
 
+        // 尝试对系统服务器进行去优化处理，以改变特定方法的行为
         try {
             deoptimizeSystemServer(classLoader);
         } catch (Throwable t) {
             log("deoptimize system server failed", t);
         }
+        // 尝试钩子WindowState相关方法，以影响窗口安全属性的判断
         try {
             hookWindowState(classLoader);
         } catch (Throwable t) {
             log("hook WindowState failed", t);
         }
 
+        // 钩子HyperOS相关方法，实现特定功能
         try {
             hookHyperOS(classLoader);
         } catch (ClassNotFoundException ignored) {
@@ -47,6 +65,7 @@ public class DisableFlagSecure extends XposedModule {
             log("hook HyperOS failed", t);
         }
 
+        // 在特定Android版本上，额外钩子ActivityTaskManagerService
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             try {
                 hookActivityTaskManagerService(classLoader);
@@ -56,12 +75,19 @@ public class DisableFlagSecure extends XposedModule {
         }
     }
 
+    /**
+     * 包加载时的处理逻辑。
+     * 主要用于对特定应用包名的处理，如Flyme和Oplus的截图功能的定制修改。
+     *
+     * @param param 包含包名和类加载器的对象。
+     */
     @SuppressLint("PrivateApi")
     @Override
     public void onPackageLoaded(@NonNull PackageLoadedParam param) {
-        if (!param.isFirstPackage()) return;
+        if (!param.isFirstPackage()) return; // 仅处理第一个加载的包
 
         var classLoader = param.getClassLoader();
+        // 根据不同的包名，执行不同的钩子逻辑
         switch (param.getPackageName()) {
             case "com.flyme.systemuiex":
                 try {
@@ -78,6 +104,7 @@ public class DisableFlagSecure extends XposedModule {
                 }
                 break;
             default:
+                // 默认尝试对Activity的onResume方法进行钩子，展示提示信息
                 try {
                     hookOnResume();
                 } catch (Throwable ignored) {
@@ -85,7 +112,15 @@ public class DisableFlagSecure extends XposedModule {
         }
     }
 
+    /**
+     * 对系统服务器相关类的方法进行去优化处理，
+     * 以改变其默认行为，达到禁用安全标志的目的。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     */
     private void deoptimizeSystemServer(ClassLoader classLoader) throws ClassNotFoundException {
+        // 对WindowStateAnimator和WindowManagerService的特定方法进行去优化处理
         deoptimizeMethods(
                 classLoader.loadClass("com.android.server.wm.WindowStateAnimator"),
                 "createSurfaceLocked");
@@ -94,6 +129,7 @@ public class DisableFlagSecure extends XposedModule {
                 classLoader.loadClass("com.android.server.wm.WindowManagerService"),
                 "relayoutWindow");
 
+        // 对外部合成Lambda类进行遍历和去优化处理
         for (int i = 0; i < 20; i++) {
             try {
                 var clazz = classLoader.loadClass("com.android.server.wm.RootWindowContainer$$ExternalSyntheticLambda" + i);
@@ -112,16 +148,31 @@ public class DisableFlagSecure extends XposedModule {
         }
     }
 
+    /**
+     * 对指定类的特定方法进行去优化处理。
+     *
+     * @param clazz 要处理的类。
+     * @param names 要去优化的方法名列表。
+     */
     private void deoptimizeMethods(Class<?> clazz, String... names) {
         var list = Arrays.asList(names);
+        // 筛选出方法名匹配的方法并进行去优化处理
         Arrays.stream(clazz.getDeclaredMethods())
                 .filter(method -> list.contains(method.getName()))
                 .forEach(this::deoptimize);
     }
 
+    /**
+     * 钩子WindowState类的相关方法，以修改窗口的安全性判断。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     * @throws NoSuchMethodException  如果指定方法未找到。
+     */
     private void hookWindowState(ClassLoader classLoader) throws ClassNotFoundException, NoSuchMethodException {
         var windowStateClazz = classLoader.loadClass("com.android.server.wm.WindowState");
         Method isSecureLockedMethod;
+        // 根据Android版本选择合适的方法进行钩子
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             isSecureLockedMethod = windowStateClazz.getDeclaredMethod("isSecureLocked");
         } else {
@@ -131,6 +182,13 @@ public class DisableFlagSecure extends XposedModule {
         hook(isSecureLockedMethod, ReturnFalseHooker.class);
     }
 
+    /**
+     * 在Android特定版本上钩子ActivityTaskManagerService相关方法。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     * @throws NoSuchMethodException  如果指定方法未找到。
+     */
     @TargetApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     private void hookActivityTaskManagerService(ClassLoader classLoader) throws ClassNotFoundException, NoSuchMethodException {
         var activityTaskManagerServiceClazz = classLoader.loadClass("com.android.server.wm.ActivityTaskManagerService");
@@ -140,34 +198,68 @@ public class DisableFlagSecure extends XposedModule {
         hook(method, ReturnNullHooker.class);
     }
 
+    /**
+     * 钩子HyperOS相关方法，以实现特定功能。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     */
     private void hookHyperOS(ClassLoader classLoader) throws ClassNotFoundException {
         var windowManagerServiceImplClazz = classLoader.loadClass("com.android.server.wm.WindowManagerServiceImpl");
         hookMethods(windowManagerServiceImplClazz, ReturnFalseHooker.class, "notAllowCaptureDisplay");
     }
 
+    /**
+     * 钩子Flyme相关方法，以禁用其截图中的安全层检查。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     * @throws NoSuchMethodException  如果指定方法未找到。
+     */
     private void hookFlyme(ClassLoader classLoader) throws ClassNotFoundException, NoSuchMethodException {
         var screenshotHardwareBufferClazz = classLoader.loadClass("android.view.SurfaceControl$ScreenshotHardwareBuffer");
         var method = screenshotHardwareBufferClazz.getDeclaredMethod("containsSecureLayers");
         hook(method, ReturnFalseHooker.class);
     }
 
+    /**
+     * 钩子Oplus相关方法，以禁用其截图功能中的特定逻辑。
+     *
+     * @param classLoader 要处理的类的类加载器。
+     * @throws ClassNotFoundException 如果指定类未找到。
+     * @throws NoSuchMethodException  如果指定方法未找到。
+     */
     private void hookOplus(ClassLoader classLoader) throws ClassNotFoundException, NoSuchMethodException {
         var screenshotContextClazz = classLoader.loadClass("com.oplus.screenshot.screenshot.core.ScreenshotContext");
         hookMethods(screenshotContextClazz, ReturnNullHooker.class, "setScreenshotReject", "setLongshotReject");
     }
 
-    private void hookMethods(Class<?> clazz, Class<? extends Hooker> hooker, String... names) {
-        var list = Arrays.asList(names);
-        Arrays.stream(clazz.getDeclaredMethods())
-                .filter(method -> list.contains(method.getName()))
-                .forEach(method -> hook(method, hooker));
-    }
-
+    /**
+     * 钩子所有Activity的onResume方法，以展示特定的Toast信息。
+     *
+     * @throws NoSuchMethodException 如果指定方法未找到。
+     */
     private void hookOnResume() throws NoSuchMethodException {
         var method = Activity.class.getDeclaredMethod("onResume");
         hook(method, ToastHooker.class);
     }
 
+    /**
+     * 对指定类的特定方法应用钩子，使用给定的Hooker类处理。
+     *
+     * @param clazz  要处理的类。
+     * @param hooker 用于处理方法调用的Hooker类。
+     * @param names  要钩子的方法名列表。
+     */
+    private void hookMethods(Class<?> clazz, Class<? extends Hooker> hooker, String... names) {
+        var list = Arrays.asList(names);
+        // 筛选出方法名匹配的方法并应用钩子
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> list.contains(method.getName()))
+                .forEach(method -> hook(method, hooker));
+    }
+
+    // 定义一个空的Hooker接口，用于在方法调用前执行特定逻辑
     @XposedHooker
     private static class ReturnFalseHooker implements Hooker {
         @BeforeInvocation
@@ -176,6 +268,7 @@ public class DisableFlagSecure extends XposedModule {
         }
     }
 
+    // 定义一个空的Hooker接口，用于在方法调用前返回null
     @XposedHooker
     private static class ReturnNullHooker implements Hooker {
         @BeforeInvocation
@@ -184,6 +277,7 @@ public class DisableFlagSecure extends XposedModule {
         }
     }
 
+    // 定义一个空的Hooker接口，用于在Activity的onResume方法调用前展示Toast信息并结束当前Activity
     @XposedHooker
     private static class ToastHooker implements Hooker {
         @BeforeInvocation
